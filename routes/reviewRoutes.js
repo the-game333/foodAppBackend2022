1f7b72d880e46baa91047a0ebdadf62b8b26451b
Abhay <abhaypawar596@gmail.com>
2022-11-27 21:06:56 +0530

    ..

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,8 +4,9 @@ const { createReviewController, getAllReviewController
 } =
     require('../controller/reviewController');
 // plans -> get all the plans from db -> sensitive route -> protected route -> logged in i will only allow that person 
-reviewRoutes.route("/")
-    .get(getAllReviewController)
-    .post(createReviewController)
+reviewRoutes.post(createReviewController);
+    //.route("/")
+    //.get(getAllReviewController)
+    
 // loggedin plan
 module.exports = reviewRoutes;
