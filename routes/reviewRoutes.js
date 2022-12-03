c0272b389e120e59892bbf5a2d87454f493f7a46
Abhay <abhaypawar596@gmail.com>
2022-12-03 11:56:14 +0530

    ..

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const reviewRoutes = express.Router();
-const { createReviewController, getAllReviewController
+const { createReviewController, getAllReviewController, getTop3Reviews
 } =
     require('../controller/reviewController');
 // plans -> get all the plans from db -> sensitive route -> protected route -> logged in i will only allow that person
